d383988bd234b3830c80836da74829c218bedd4a
Your Name <your.email@example.com>
2025-07-13 11:33:18 -0400

    test3

--- config.h
+++ config.h
@@ -4,10 +4,10 @@ static const Block blocks[] = {
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	/* {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
 	{"",	"sb-battery",	5,	3},
-	{"",	"sb-volume",	0,	10},
+	{"",	"sb-volume",	1,	10},
 	{"",	"sb-clock",		60,	1},
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-brightness",	0,	11},
+	{"",	"sb-brightness",	1,	11},
 	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-cpu",		10,	18}, */
 	/* {"",	"sb-tasks",	10,	26}, */
