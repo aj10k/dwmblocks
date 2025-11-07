c32679a9b7f360b3ed855cf2e1fc990210801294
aj10k <28464125+aj10k@users.noreply.github.com>
2025-11-07 10:33:29 -0500

    nix-shell

---
+++ shell.nix
@@ -0,0 +1,43 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    # Build tools
+    gcc
+    gnumake
+    pkg-config
+    
+    # X11 libraries and headers
+    xorg.libX11
+    xorg.libX11.dev
+    xorg.libXinerama
+    xorg.libXft
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXext
+    xorg.xorgproto
+    
+    # Font libraries
+    fontconfig
+    fontconfig.dev
+    freetype
+    freetype.dev
+    
+    # Additional libraries that dwm might need
+    xorg.libxcb
+    xorg.xcbutil
+    xorg.xcbutilwm
+    xorg.xcbutilimage
+    xorg.xcbutilkeysyms
+    xorg.xcbutilrenderutil
+  ];
+  
+  shellHook = ''
+    echo "DWM development environment loaded"
+    echo "X11 libraries and headers are now available"
+    echo "Try: make clean && make"
+    make clean && make
+
+    exit 0
+  '';
+}
